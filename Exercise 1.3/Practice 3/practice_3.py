38d1a3c9912b132a9e4f3c2b75624ed5fb2528d6
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-07-17 17:13:54 -0700

    wrote a for and while loop

---
+++ Exercise 1.3/Practice 3/practice_3.py
@@ -0,0 +1,13 @@
+# for loop
+
+for num in range(10, 51, 10):
+    print(num)
+print("And we are done!")
+
+# while loop
+
+num = 10
+while num <= 50:
+    print(num)
+    num += 10
+print("And we are done!")
