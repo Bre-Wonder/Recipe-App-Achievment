9834daaa63717a699e11daf6d941d5d5fe227eb8
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-07-18 16:35:33 -0700

    added function and global variable

--- Exercise 1.3/Exercise_1.3.py
+++ Exercise 1.3/Exercise_1.3.py
@@ -1,3 +1,17 @@
 recipe_list = []
-
 ingredients_list = []
+
+n = int(input("How many recipes would you like to have? "))
+
+
+def take_recipe():
+    name = str(input("Please give the name of the recipe here: "))
+    cooking_time = int(input("Please give the cooking time in minutes: "))
+    ingredients = input(
+        "Please list your ingredients here (please note to put a comma between each ingredient): ").split(', ')
+    recipe = {'name': name, 'cooking_time': cooking_time,
+              'ingredients': ingredients}
+    print(recipe)
+
+
+take_recipe()
