833e30fd926d8a261093db5bf70367c48d1b6e2b
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-07-24 12:20:32 -0700

    added two functions: take_recipe + calc_difficulty

--- Exercise 1.4/recipe_input.py
+++ Exercise 1.4/recipe_input.py
@@ -1,1 +1,28 @@
 import pickle
+
+# allows user to input a recipe of choice
+
+
+def take_recipe():
+    recipe_name = str(input("Please give the name of the recipe here: "))
+    cooking_time = int(input("Please give the cooking time in minutes: "))
+    ingredients = input(
+        "Please list your ingredients here (please note to put a comma between each ingredient): ").split(', ')
+    difficulty = calc_difficulty()
+    recipe = {'recipe_name': recipe_name, 'cooking_time': cooking_time,
+              'ingredients': ingredients, 'difficulty': difficulty}
+    return recipe
+
+# function that determines how difficult the recipe is
+
+
+def calc_difficulty():
+    if recipe['cooking_time'] < 10 and len(recipe['ingredients']) < 4:
+        difficulty = 'Easy'
+    elif recipe['cooking_time'] < 10 and len(recipe['ingredients']) >= 4:
+        difficulty = 'Medium'
+    elif recipe['cooking_time'] >= 10 and len(recipe['ingredients']) < 4:
+        difficulty = 'Intermediate'
+    elif recipe['cooking_time'] >= 10 and len(recipe['ingredients']) >= 4:
+        difficulty = 'Hard'
+    recipe['difficulty'] = difficulty
