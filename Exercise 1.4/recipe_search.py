dcf6970a55398d5a8fcfabb0d84040cf547c0a92
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-07-25 12:31:13 -0700

    added input for file name and started search_ingredients function

--- Exercise 1.4/recipe_search.py
+++ Exercise 1.4/recipe_search.py
@@ -7,3 +7,18 @@ def display_recipe(recipe):
     print(f"Cooking Time (in minutes): {recipe['cooking_time']}")
     print(f"Ingredients: " + ', '.join(recipe['ingredients']))
     print(f"Difficulty Level: {recipe['difficulty']}")
+
+
+def search_ingredients(data):
+    print('All Ingredients ---')
+    for index, ingredient in enumerate(data['all_ingredients']):
+        print(f"{index}: {ingredient}")
+
+
+named_file_2 = input(
+    'What is the name of the file where you would like to store the data for your recipes? ')
+if not named_file_2.endswith('.bin'):
+    named_file_2 += '.bin'
+    print('Your file name did not include .bin at the end. It has been added to your file name.')
+else:
+    print('Name convention done correctly')
