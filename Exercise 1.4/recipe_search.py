fe48664c8426e2e0543e56ba32dc8a379f401e73
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-07-25 14:51:33 -0700

    added try-except block to check for errors

--- Exercise 1.4/recipe_search.py
+++ Exercise 1.4/recipe_search.py
@@ -14,6 +14,29 @@ def search_ingredients(data):
     for index, ingredient in enumerate(data['all_ingredients']):
         print(f"{index}: {ingredient}")
 
+    try:
+        index_number_chosen = int(input(
+            'Please select number from index of ingredients: '))
+        ingredient_searched = data['all_ingredients'][index_number_chosen]
+
+    except ValueError:
+        print('Value must be a number')
+
+    except IndexError:
+        print('That index number does not exist in you current index')
+
+    else:
+        matching_recipe = []
+        for recipe in data['recipe_list']:
+            if ingredient_searched in recipe['ingredients']:
+                matching_recipe.append(recipe['recipe_name'])
+
+        if matching_recipe:
+            for name in matching_recipe:
+                print(name)
+        else:
+            print('No recipes contain this ingredient')
+
 
 named_file_2 = input(
     'What is the name of the file where you would like to store the data for your recipes? ')
@@ -22,3 +45,13 @@ if not named_file_2.endswith('.bin'):
     print('Your file name did not include .bin at the end. It has been added to your file name.')
 else:
     print('Name convention done correctly')
+
+try:
+    my_file = open(named_file_2, )
+
+# runs an error if file is not found in the correct file path or not found at all
+except FileNotFoundError:
+    print('File not found, please select another file')
+
+else:
+    search_ingredients(data)
