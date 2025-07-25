2e8df947c51293e72f938e67674e2efdffe2890f
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-07-25 12:06:34 -0700

    added display_recipe function

--- Exercise 1.4/recipe_search.py
+++ Exercise 1.4/recipe_search.py
@@ -1,1 +1,9 @@
 import pickle
+
+
+def display_recipe(recipe):
+    print('Recipe Information ---')
+    print(f"Recipe: {recipe['recipe_name']}")
+    print(f"Cooking Time (in minutes): {recipe['cooking_time']}")
+    print(f"Ingredients: " + ', '.join(recipe['ingredients']))
+    print(f"Difficulty Level: {recipe['difficulty']}")
