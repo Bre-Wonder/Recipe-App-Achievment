95f3cd75ac7b333ed4c871a00d628e0a065c6f8f
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-07-28 12:44:11 -0700

    updated methods to behave more cleanly

--- Exercise 1.5/shopping_list.py
+++ Exercise 1.5/shopping_list.py
@@ -1,20 +1,24 @@
 class ShoppingList(object):
 
     # initialized data attributes
-    __init__(self, list_name):
+    def __init__(self, list_name):
 
         # name of the shopping list
-    self.list_name = name of the shopping list
+        self.list_name = list_name
 
     # a list that will contain names of items
-    self.shopping_list = []
+        self.shopping_list = []
 
     # adds an item to a self.shopping_list if item is not already there
-    add_item(self, item):
+    def add_item(self, item):
+        if item not in self.shopping_list:
+            self.shopping_list.append(item)
 
-        # removes an item from shopping_list
-    remove_item(self, item):
+            # removes an item from shopping_list
+    def remove_item(self, item):
+        if item in self.shopping_list:
+            self.shopping_list.remove(item)
 
-        # prints the contents of self.shopping_list
-    view_list(self):
+            # prints the contents of self.shopping_list
+    def view_list(self):
         print(self.shopping_list)
