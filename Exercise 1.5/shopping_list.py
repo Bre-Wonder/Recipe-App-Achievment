a340615862c2b4879e92c7804aaad71b9e87e500
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-07-28 12:34:01 -0700

    created class with method

---
+++ Exercise 1.5/shopping_list.py
@@ -0,0 +1,20 @@
+class ShoppingList(object):
+
+    # initialized data attributes
+    __init__(self, list_name):
+
+        # name of the shopping list
+    self.list_name = name of the shopping list
+
+    # a list that will contain names of items
+    self.shopping_list = []
+
+    # adds an item to a self.shopping_list if item is not already there
+    add_item(self, item):
+
+        # removes an item from shopping_list
+    remove_item(self, item):
+
+        # prints the contents of self.shopping_list
+    view_list(self):
+        print(self.shopping_list)
