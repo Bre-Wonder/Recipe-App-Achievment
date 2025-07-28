a5b9913d35e92b702216b695b304f7834ee90369
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-07-28 15:30:02 -0700

    wrote out two methods

---
+++ Exercise 1.5/Practice 2/height_measure.py
@@ -0,0 +1,8 @@
+class Heigh(object):
+    def __init__(self, feet, inches):
+        self.feet = feet
+        self.inches = inches
+
+    def __str__(self):
+        output = str(self.feet) + " feet, " + str(self.inches) + " inches"
+        return output
