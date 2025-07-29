00fffe34eb1692dd890faae872a857ab54d3ec3c
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-07-29 10:38:55 -0700

    added method calculate_difficulty

--- Exercise 1.5/recipe_oop.py
+++ Exercise 1.5/recipe_oop.py
@@ -1,6 +1,16 @@
 class Recipe(object):
     def __init__(self, name, ingredients, cooking_time):
         self.name = name
-        self.ingredients = ingredients
+        # self.ingredients = ingredients[] make ingredients into a list
         self.cooking_time = cooking_time
         self.difficulty = self.calculate_difficulty()
+
+    def calculate_difficulty(self, cooking_time, ingredients, difficulty):
+        if cooking_time < 10 and len(ingredients) < 4:
+            return 'Easy'
+        elif cooking_time < 10 and len(ingredients) >= 4:
+            return 'Medium'
+        elif cooking_time >= 10 and len(ingredients) < 4:
+            return 'Intermediate'
+        elif cooking_time >= 10 and len(ingredients) >= 4:
+            return 'Hard'
