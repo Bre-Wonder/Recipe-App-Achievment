daa9bc99b23553c8c0c606c15c3efa23340c730f
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-07-29 10:31:53 -0700

    created class and initialization

--- Exercise 1.5/recipe_oop.py
+++ Exercise 1.5/recipe_oop.py
@@ -1,1 +1,6 @@
 class Recipe(object):
+    def __init__(self, name, ingredients, cooking_time):
+        self.name = name
+        self.ingredients = ingredients
+        self.cooking_time = cooking_time
+        self.difficulty = self.calculate_difficulty()
