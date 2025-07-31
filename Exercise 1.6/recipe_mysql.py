57dd428d035eb7e1d200373b72994d0f616817bc
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-07-31 16:01:04 -0700

    created main menu function

--- Exercise 1.6/recipe_mysql.py
+++ Exercise 1.6/recipe_mysql.py
@@ -21,3 +21,43 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS recipes(
   cooking_time INT,
   difficulty VARCHAR(20)
 )''')
+
+
+def main_menu(conn, cursor):
+    while (choice != 'quit'):
+        print('MAIN MENU')
+        print('----------------------')
+        print('----------------------')
+        print('What would you like to do? Pick a number that corresponds with the option you would like to select')
+        print('1. Create a recipe')
+        print('2. Search for a recipe')
+        print('3. Update a recipe')
+        print('4. Delete a recipe')
+        print('Type 'quit' to exit the program')
+        choice = input('Your Choice: ')
+
+        if choice == '1':
+            create_recipe(conn, cursor)
+        elif choice == '2':
+            search_recipe(conn, cursor)
+        elif choice == '3':
+            update_recipe(conn, cursor)
+        elif choice == '4':
+            delete_recipe(conn, cursor)
+        else:
+            print('Selection not valid. Please select another number')
+
+
+main_menu(conn, cursor)
+
+
+def create_recipe(conn, cursor):
+
+
+def search_recipe(conn, cursor):
+
+
+def update_recipe(conn, cusor):
+
+
+def delete_recipe(conn, cursor):
