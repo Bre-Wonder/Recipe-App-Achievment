154a441db33b38d2652c71d916bff354369d6599
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-11 15:17:04 -0700

    added table columns to model class

--- Acheivement 2/Exercise 2.2/practice - bookstore src/salespersons/models.py
+++ Acheivement 2/Exercise 2.2/practice - bookstore src/salespersons/models.py
@@ -3,4 +3,10 @@ from django.db import models
 # Create your models here.
 
 
-# class Salesperson(models.Model):
+class Salesperson(models.Model):
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    bio = models.TextField(default="no bio...")
+
+    def __str__(self):
+        return str(self.name)
