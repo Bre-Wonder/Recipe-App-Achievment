0c6634cc16618f1cb3312d45030ff43f3555029c
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-11 19:51:36 -0700

    ran migration packaging and migrated to update database

--- Acheivement 2/A2_Recipe_App/src/recipeApp/models.py
+++ Acheivement 2/A2_Recipe_App/src/recipeApp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Recipe(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    ingredients = models.VarCHAR(max_length=255)
+    ingredients = models.CharField(max_length=255)
     cooking_time = models.IntegerField()
     difficulty = models.CharField(max_length=20)
     description = models.TextField()
