3c672e696802c67b436958e544a87234532afc8b
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-11 19:59:14 -0700

    took out difficulty becasue the will be calculate later

--- Acheivement 2/A2_Recipe_App/src/recipeApp/models.py
+++ Acheivement 2/A2_Recipe_App/src/recipeApp/models.py
@@ -6,7 +6,6 @@ class Recipe(models.Model):
     name = models.CharField(max_length=50)
     ingredients = models.CharField(max_length=255)
     cooking_time = models.IntegerField()
-    difficulty = models.CharField(max_length=20)
     description = models.TextField()
 
     def __str__(self):
