2d87e1f932c2ac61f2c57901c0c25ad25d0cec0d
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-11 19:39:13 -0700

    wrote model class for Users app

--- Acheivement 2/A2_Recipe_App/src/users/models.py
+++ Acheivement 2/A2_Recipe_App/src/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class User(models.Model):
+    id =
+    username = models.CharField(max_length=120)
+
+    def __str__(self):
+        return str(self.name)
+
 # Create your models here.
