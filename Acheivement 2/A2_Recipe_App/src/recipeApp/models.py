b85f9013cf90e984e926705fc1803cd4a26286dd
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-11 19:39:01 -0700

    wrote model class for recipeApp

--- Acheivement 2/A2_Recipe_App/src/recipeApp/models.py
+++ Acheivement 2/A2_Recipe_App/src/recipeApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
+class Recipe(models.Model):
+    id =
+    name = models.CharField(max_length=120)
+    ingredients =
+    cooking_time =
+    difficulty =
+    description = models.TextField()
+
+    def __str__(self):
+        return str(self.name)
+
 # Create your models here.
