2e2436ec673e76b9c478ce4e65bb447673e6a174
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-13 16:08:52 -0700

    added pic and difficulty attribute

--- Acheivement 2/A2_Recipe_App/src/recipeApp/models.py
+++ Acheivement 2/A2_Recipe_App/src/recipeApp/models.py
@@ -6,7 +6,10 @@ class Recipe(models.Model):
     name = models.CharField(max_length=50)
     ingredients = models.CharField(max_length=255)
     cooking_time = models.IntegerField()
+    difficulty = models.CharField(max_length=25)
     description = models.TextField()
+    # come back to define "upload_to"
+    pic = pic = models.ImageField(upload_to='', default='no_picture.jpg')
 
     def __str__(self):
         return str(self.name)
