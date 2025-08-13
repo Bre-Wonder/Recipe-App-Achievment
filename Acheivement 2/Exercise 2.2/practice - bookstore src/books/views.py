f5fd1faf8e3a8b384e538f372c4e4c3fe10ee281
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-13 11:50:36 -0700

    created class based view called BookListView

--- Acheivement 2/Exercise 2.2/practice - bookstore src/books/views.py
+++ Acheivement 2/Exercise 2.2/practice - bookstore src/books/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+# came with the file
+from django.views.generic import ListView
+# displays a list of books
+from .models import Book
+# accessing book model
 
 # Create your views here.
+
+
+class BookListView(ListView):
+    # specify which model
+    model = Book
+    # specifies which template
+    template_name = 'books/main.html'
