3b8e351c9198dbd866ebd9c041b07a8ed7ce490d
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-12 18:57:12 -0700

    add image as table value in the Book class

--- Acheivement 2/Exercise 2.2/practice - bookstore src/books/models.py
+++ Acheivement 2/Exercise 2.2/practice - bookstore src/books/models.py
@@ -37,5 +37,7 @@ class Book(models.Model):
 
     author_name = models.CharField(max_length=120)
 
+    pic = models.ImageField(upload_to='books', default='no_picture.jpg')
+
     def __str__(self):
         return str(self.name)
