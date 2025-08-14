112a7563a8c8f3c3c9ef0ccccd459202ba8c8307
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-14 13:54:24 -0700

    created a class based view

--- Acheivement 2/A2_Recipe_App/src/recipeApp/views.py
+++ Acheivement 2/A2_Recipe_App/src/recipeApp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Recipe
 
 # Create your views here.
+
+
+class RecipeListView(ListView):
+    # model that is being communicated with
+    model = Recipe
+    # template that is being used
+    template_name = 'recipeApp/main_recipelist.html'
 
 
 def home(request):
