d5c5a155d183b8f151f14d967e8a51598ad658e8
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-15 14:49:37 -0700

    imported logout ot he view and defined a function for logout

--- Acheivement 2/Exercise 2.2/practice - bookstore src/bookstore/views.py
+++ Acheivement 2/Exercise 2.2/practice - bookstore src/bookstore/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 # libraries that django has for authentication
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 # form django provides for authentication
 from django.contrib.auth.forms import AuthenticationForm
 
@@ -35,3 +35,8 @@ def login_view(request):
     }
 
     return render(request, 'auth/login.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(login)
