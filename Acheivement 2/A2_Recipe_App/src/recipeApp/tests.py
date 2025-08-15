27f5ac47fad5a2f31f2e61190852d1b0613a205b
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-14 20:47:59 -0700

    added test

--- Acheivement 2/A2_Recipe_App/src/recipeApp/tests.py
+++ Acheivement 2/A2_Recipe_App/src/recipeApp/tests.py
@@ -51,3 +51,8 @@ class RecipeModelTest(TestCase):
 
         # Compare the value to the expected result
         self.assertEqual(field_label, 'description')
+
+    def test_get_absolute_url(self):
+        recipe = Recipe.objects.get(id=1)
+        # get_absolute_url() should take you to the detail page of recipe #1
+        self.assertEqual(recipe.get_absolute_url(), '/books/list/1')
