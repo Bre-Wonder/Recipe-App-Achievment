e5db8b40114b15c1f7e9ef49b82a6ec63d85169b
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-15 21:22:21 -0700

    added name for home url path

--- Acheivement 2/A2_Recipe_App/src/recipeApp/urls.py
+++ Acheivement 2/A2_Recipe_App/src/recipeApp/urls.py
@@ -4,7 +4,7 @@ from .views import home, RecipeListView, RecipeDetailView
 app_name = 'recipeApp'
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
     path('list/', RecipeListView.as_view(), name='list'),
     path('list/<pk>', RecipeDetailView.as_view(), name='detail')
 ]
