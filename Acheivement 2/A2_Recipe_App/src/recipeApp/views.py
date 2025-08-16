409991e3a47da04581a8bb8cce348fba11f11078
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-15 21:22:41 -0700

    updated login protection for classes

--- Acheivement 2/A2_Recipe_App/src/recipeApp/views.py
+++ Acheivement 2/A2_Recipe_App/src/recipeApp/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     # model that is being communicated with
     model = Recipe
     # template that is being used
     template_name = 'recipeApp/main_recipelist.html'
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
   # model that is being communicated with
     model = Recipe
     # template that is being used
