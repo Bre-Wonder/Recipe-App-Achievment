e01442e2cf35916eed92ceabd4707865a629eb58
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-16 15:21:57 -0700

    imported get_bookname_from_id function

--- Acheivement 2/Exercise 2.2/practice - bookstore src/sales/views.py
+++ Acheivement 2/Exercise 2.2/practice - bookstore src/sales/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from .forms import SalesSearchForm
 from .models import Sale
 import pandas as pd
+from .utils import get_bookname_from_id
 
 # Create your views here.
 
@@ -18,7 +19,6 @@ def records(request):
     form = SalesSearchForm(request.POST or None)
     # initializ datafram to None
     sales_df = None
-    chart = None
 
     # check if button is clicked
     if request.method == 'POST':
@@ -27,7 +27,9 @@ def records(request):
         qs = Sale.objects.filter(book__name=book_title)
         if qs:
             sales_df = pd.DataFrame(qs.values())
-        sales_df = sales_df.to_html
+            sales_df['book_id'] = sales_df['book_id'].apply(
+                get_bookname_from_id)
+            sales_df = sales_df.to_html
 
         # commented out because not currently being used
         # print(book_title, chart_type)
