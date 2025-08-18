734600755162127fa9ec87e39b2b08ea2315679f
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-18 12:43:08 -0700

    added chart class

--- Acheivement 2/A2_Recipe_App/src/recipeApp/forms.py
+++ Acheivement 2/A2_Recipe_App/src/recipeApp/forms.py
@@ -1,17 +1,20 @@
 from django import forms
 
-# comment out for now until I know I want to use this
-# CHART_CHOICES = (
-#   ('#1', 'Bar Chart'),
-#   ('#2', 'Pie Chart'),
-#   ('#3', 'Line Chart')
-# )
+comment out for now until I know I want to use this
+CHART_CHOICES = (
+    ('#1', 'Bar Chart'),
+    ('#2', 'Pie Chart'),
+    ('#3', 'Line Chart')
+)
 
 # class created for form to be used
 
 
 class IngredientSearchForm(forms.Form):
-    # may need to be redifined later, correctly for recipe_ingredients
+    # defined form type
     recipe_title = forms.CharField(max_length=120, label="Recipe Lookup")
-    # only use this if I want them to appear on the same page - comment out for now
-    # chart_type = forms.ChoiceField(choices=CHART_CHOICES)
+
+
+class ChartForm(forms.Form):
+    # create chart form and added choices
+    chart_type = forms.ChoiceField(choices=CHART_CHOICES)
