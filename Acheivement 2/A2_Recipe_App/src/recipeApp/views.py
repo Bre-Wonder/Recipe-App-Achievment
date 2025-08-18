ffcb286c08b336d4096ea7617070a480312bb0ce
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-08-17 18:21:42 -0700

    added form function

--- Acheivement 2/A2_Recipe_App/src/recipeApp/views.py
+++ Acheivement 2/A2_Recipe_App/src/recipeApp/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
 from django.contrib.auth.mixins import LoginRequiredMixin
+# import form from forms.py
+from .forms import IngredientSearchForm
+
+
 # Create your views here.
 
 
@@ -10,6 +14,11 @@ class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     # template that is being used
     template_name = 'recipeApp/main_recipelist.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = IngredientSearchForm()
+        return context
 
 
 class RecipeDetailView(LoginRequiredMixin, DetailView):
@@ -21,3 +30,17 @@ class RecipeDetailView(LoginRequiredMixin, DetailView):
 
 def home(request):
     return render(request, 'recipeApp/recipe_home.html')
+
+ # function that send in form for the user
+
+
+def IngredientSearch(request):
+  # creates an instance of the form
+    form = IngredientSearchForm(request.POST or None)
+
+    # packs up dtat to be sent to template in the form of a dictionary
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'recipeApp/ingredient_search.html', context)
